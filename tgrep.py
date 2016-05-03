9385bff19dfcfc11f19d734a64e66f75edec3a8f
Joe Monaco <self@joemona.co>
2016-05-03 11:31:58 -0400

    More Pythonic rewrite
    - Add match highlighting
    - Preserve indentation of match results
    - More flexible input arguments: files, directories, or combination
    - Default to current directory with no arguments
    - Faster file reading using line generator

--- tgrep.py
+++ tgrep.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-"""tree_search.py -- search a directory tree for text
-   Usage: tree_search.py topdir searchterm
 
-   Written by Joe Monaco, 3/14/2007
-   Center for Theoretical Neuroscience
 """
+Linewise recursive search for a pattern in text files ("tree grep").
+
+(I wrote this before discovering `ack`. It's basically the same.)
+
+Author: Joe Monaco
+Created: 03-14-2007
+Updated: 05-03-2016
+"""
+
+from __future__ import print_function
 
 import os
 import sys
@@ -12,50 +18,85 @@ import re
 
 exclude_types = ['o','so','a','dylib','pdf','pyo','pyc','nib','plist',
     'rsrc','icns','gz','bz2','zip','tar','dmg','bundle','app']
+
 exclude_dirs = ['bundle','app','pbxindex','pbxstrings','build','svn','git']
+
+USAGE = "Usage: {} pattern [dir-or-file [...]]"
+
 
 def blue(s):
     return '\033[0;36m' + s + '\033[0m'
 
-def gray(s):
+def white(s):
     return '\033[0;37m' + s + '\033[0m'
 
-def tree_search(text, topdir):
-    "walk the tree, searching for text"
-    td = os.path.realpath(topdir)
-    if not os.path.exists(td):
-        sys.stderr.write('Directory not found\n')
-        return
-    for d, dn, fn in os.walk(td):
-        for i, subd in enumerate(dn):
+def gray(s):
+    return '\033[1;36m' + s + '\033[0m'
+
+def highlight(s):
+    return '\033[30;43m' + s + '\033[0m'
+
+
+def tgrep_all(pattern, target_list):
+    """
+    Search all target arguments in target list for pattern.
+    """
+    for dir_or_file in target_list:
+        if os.path.isdir(dir_or_file):
+            tgrep(pattern, os.walk(dir_or_file))
+        elif os.path.isfile(dir_or_file):
+            head, tail = os.path.split(dir_or_file)
+            targets = [(head, [], [tail])]
+            tgrep(pattern, targets)
+    return 0
+
+def tgrep(pattern, targets):
+    """
+    Search target files for pattern and print colorized matches
+    to the terminal.
+    """
+    for dirpath, dirnames, filenames in targets:
+        for i, subd in enumerate(dirnames):
             if subd.split('.')[-1] in exclude_dirs:
-                del dn[i]
-        for f in fn:
-            if f.split('.')[-1] in exclude_types:
+                del dirnames[i]
+
+        for fn in filenames:
+            if fn.split('.')[-1] in exclude_types:
                 continue
-            f_lines = []
-            try:
-                fd = open(os.path.join(d, f), 'r')
-            except IOError: continue
-            else:
-                f_lines = fd.readlines()
-                fd.close()
-            if len(f_lines) > 10000:
-                continue
-            l = 0
-            once = False
-            while f_lines:
-                l += 1
-                theline = f_lines.pop(0)
-                if len(theline) > 255: continue
-                if re.search(text, theline, flags=re.I):
-                    if not once:
-                        sys.stdout.write(blue(os.path.join(d, f)) + '\n')
-                        once = True
-                    sys.stdout.write(gray('%4d '%(l,)) + theline.strip() + '\n')
+
+            lineno = 0
+            printed_path = False
+            pth = os.path.join(dirpath, fn)
+            with open(pth, 'r') as f:
+                for line in f:
+                    lineno += 1
+                    match = re.search(pattern, line, flags=re.I)
+                    if match is None:
+                        continue
+                    if not printed_path:
+                        print(blue(pth))
+                        printed_path = True
+                    start, end = match.span()
+                    res = white('{:4d} '.format(lineno))
+                    res += gray(line[:start])
+                    res += highlight(line[start:end])
+                    res += gray(line[end:].rstrip())
+                    print(res)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.stderr.write(__doc__)
+    head, callname = os.path.split(sys.argv[0])
+    if len(sys.argv) == 2:
+        pattern = sys.argv[1]
+        targets = [os.getcwd()]
+    elif len(sys.argv) >= 3:
+        if sys.argv[2] in ('-h', '--help'):
+            print(USAGE.format(callname))
+            sys.exit(1)
+        pattern = sys.argv[1]
+        targets = sys.argv[2:]
+    else:
+        print(USAGE.format(callname))
         sys.exit(1)
-    tree_search(sys.argv[1], sys.argv[2])
+
+    sys.exit(tgrep_all(pattern, targets))
